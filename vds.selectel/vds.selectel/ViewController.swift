61fe4ecb18f5cd1511de77235fee0d9115646c32
swiftproger <swiftapp@mail.ru>
2023-10-30 21:48:31 +0300

    init project

---
+++ vds.selectel/vds.selectel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vds.selectel
+//
+//  Created by Александр Новиков on 30.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
